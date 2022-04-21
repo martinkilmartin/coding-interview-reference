9447106122b577e44a4121970f930445929229d5
fograi <dev@fogr.ai>
2022-04-21 16:55:33 -0400

    Add test functions

--- index.js
+++ index.js
@@ -1,25 +1,74 @@
-function arrayEquals(actual, expected) {
-    return Array.isArray(actual) &&
-        Array.isArray(expected) &&
-        actual.length === expected.length &&
-        actual.every((val, index) => val === expected[index]);
+function arrayEquals(name, actual, expected) {
+  const equals =
+    Array.isArray(actual) &&
+    Array.isArray(expected) &&
+    actual.length === expected.length &&
+    actual.every((val, index) => val === expected[index]);
+  return equalsMessaging(name, equals, actual, expected);
 }
 
-function stringEquals(actual, expected) {
-    console.log(actual);
-    console.log(expected);
-    return actual == expected;
+function objectStrictlyEquals(name, actual, expected) {
+  return equalsMessaging(name, actual === expected, actual, expected);
+}
+
+function equalsMessaging(name = "test case", equals, actual, expected) {
+  if (equals) {
+    console.log(`✅ ${name}`);
+  } else {
+    console.log(`❌ ${name} failed!`);
+    console.log(`❌ expected: ${expected}`);
+    console.log(`❌ actual: ${actual}`);
+  }
+  return equals;
 }
 
 const arrayToSort = [12, 3, 56, 9, 55, 99, 177];
 
+const reverseString = (s) => s.split("").reverse().join("");
+
+function isPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    console.log(s[left], s[right]);
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+
 console.log(
-    arrayEquals(
-        arrayToSort.sort((a, b) => a - b),
-        [3, 9, 12, 55, 56, 99, 177]
-    )
+  arrayEquals(
+    "sorts array",
+    arrayToSort.sort((a, b) => a - b),
+    [3, 9, 12, 55, 56, 99, 177]
+  )
 );
 
-function reverseAString(s) {
-    
-}
+console.log(objectStrictlyEquals("test object equality comparison", "b", "b"));
+
+console.log(
+  objectStrictlyEquals("it reverses a string", reverseString("hello"), "olleh")
+);
+
+console.log(objectStrictlyEquals("it detects a pali", isPalindrome("a"), true));
+
+console.log(
+  objectStrictlyEquals("it detects a pali", isPalindrome("aa"), true)
+);
+console.log(
+  objectStrictlyEquals("it detects a pali", isPalindrome("abba"), true)
+);
+
+console.log(
+  objectStrictlyEquals("it detects a pali", isPalindrome("aimia"), true)
+);
+console.log(
+  objectStrictlyEquals("it detects a non-pali", isPalindrome("hello"), false)
+);
+console.log(
+  objectStrictlyEquals("it detects a non-pali", isPalindrome("aira"), false)
+);
